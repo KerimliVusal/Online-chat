ce4d1b19f9fb2b32deda414341f6570e7c077718
KerimliVusal <kerimlivusal98@gmail.com>
2023-02-01 22:13:09 +0400

    first commit

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express=require('express')
+const app=express()
+const http=require('http')
+const cors=require('cors')
+const {Server}=require('socket.io')
+app.use(cors());
+
+const server=http.createServer(app);
+const io=new Server(server,{
+    cors:{
+        origin:"http://localhost:3000",
+        methods :["Get","Post"]
+    },
+}) ;
+
+io.on("connection",(socket)=>{
+    console.log(` user is connected ${socket.id}`);
+socket.on("join_room",(data)=>{
+    socket.join(data)
+    console.log(` user is connected with id ${socket.id} Joined room ${data}`);
+    
+})
+socket.on("send_data",(data)=>{
+ socket.to(data.room).emit("recieve_message",data)
+})
+  socket.on("disconnect",()=>{
+    console.log('user disconnected',socket.id)
+  })
+})
+
+server.listen(3001,()=>{
+    console.log('server is runing')
+})
